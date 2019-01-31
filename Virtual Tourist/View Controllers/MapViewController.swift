982d7647d9d7cf702244333097199500bdb678f9
Felipe Ribeiro <feliperibeiro@me.com>
2019-01-31 10:54:34 -0200

    Cria primeiro view controller com o mapa

---
+++ Virtual Tourist/View Controllers/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Felipe Ribeiro on 31/01/19.
+//  Copyright © 2019 Felipe Ribeiro. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet var mapView: MKMapView!
+}
